1fde8b8b1aecabffba374fb8dc6fe8e67df39ca5
JeanDeOliveira <jeandahoraoliveira3@gmail.com>
2022-01-26 13:30:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,43 +8,45 @@ todoButton.addEventListener('click', addTodo)
 function addTodo(event){
 	event.preventDefault()
 	//div
-	if(todoInput.value === 0){
+	if(todoInput.value == 0){
 		alert('digite sua tarefa')
+		return false
+	}else{
+		const todoDiv = document.createElement('div')
+		todoList.appendChild(todoDiv)
+		todoDiv.classList.add("todo")
+
+		//li
+		const newTodo = document.createElement('li')
+		newTodo.classList.add("todo-item")
+		newTodo.innerText = todoInput.value
+		todoDiv.appendChild(newTodo)
+		
+		//checked
+		const checkButton = document.createElement('button')
+		checkButton.classList.add("complete")
+		checkButton.innerHTML = `<i class="fas fa-check"></i>`
+		todoDiv.appendChild(checkButton)
+		
+		//trash
+		const trashButton = document.createElement('button')
+		trashButton.classList.add("trash")
+		trashButton.innerHTML = `<i class="fas fa-trash"></i>`
+		todoDiv.appendChild(trashButton)
+
+
+		todoList.addEventListener('click', (e)=>{
+			const item = e.target;
+
+			if(item.classList[0] === 'trash'){
+				const todo = item.parentElement;
+				todo.remove()
+			}
+
+			if(item.classList[0] === "complete"){
+				const todo = item.parentElement;
+				todo.classList.toggle("completed")
+			}
+		})
 	}
-	const todoDiv = document.createElement('div')
-	todoList.appendChild(todoDiv)
-	todoDiv.classList.add("todo")
-
-	//li
-	const newTodo = document.createElement('li')
-	newTodo.classList.add("todo-item")
-	newTodo.innerText = todoInput.value
-	todoDiv.appendChild(newTodo)
-	
-	//checked
-	const checkButton = document.createElement('button')
-	checkButton.classList.add("complete")
-	checkButton.innerHTML = `<i class="fas fa-check"></i>`
-	todoDiv.appendChild(checkButton)
-	
-	//trash
-	const trashButton = document.createElement('button')
-	trashButton.classList.add("trash")
-	trashButton.innerHTML = `<i class="fas fa-trash"></i>`
-	todoDiv.appendChild(trashButton)
-
-
-	todoList.addEventListener('click', (e)=>{
-		const item = e.target;
-
-		if(item.classList[0] === 'trash'){
-			const todo = item.parentElement;
-			todo.remove()
-		}
-
-		if(item.classList[0] === "complete"){
-			const todo = item.parentElement;
-			todo.classList.toggle("completed")
-		}
-	})
 }
